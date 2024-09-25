a1acd1bc139a5fd4153282b605ad23381135ee25
Jamil-4khtar <156568813+Jamil-4khtar@users.noreply.github.com>
2024-09-25 21:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+let list = document.querySelector("#todoList");
+let input = document.getElementById("newTodoInput");
+let addBtn = document.getElementById("addTodoBtn");
+
+addBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	let text = input.value;
+	if (text == "") {
+		return;
+	} else {
+		let li = document.createElement("li");
+		li.innerText = text;
+		list.append(li);
+		input.value = "";
+	}
+})
 
